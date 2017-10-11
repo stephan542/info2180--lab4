90f155261bf81d3c6438dd12efffb936429a0fb2
stephan542 <stefanwytter@gmail.com>
2017-10-11 06:32:31 -0500

    Exercise 4 Alerts on successful completion of maze

--- maze.js
+++ maze.js
@@ -1,10 +1,18 @@
 // JavaScript Document
+var hitwall = false;
 
 function checkbounds(bounds){
 	"use strict";
 	bounds.forEach(function(bound){
 		bound.onmouseover = function(){
 			bound.setAttribute("class","boundary youlose");
+			hitwall = true;
+		};
+	});
+	
+	bounds.forEach(function(bound){
+		bound.onmouseout = function(){
+			bound.setAttribute("class","boundary");
 		};
 	});
 	
@@ -15,6 +23,8 @@ function init(){
 	"use strict";
 	var boundary = document.querySelectorAll(".boundary");
 	checkbounds(boundary);
+	var finish = document.getElementById("end");
+	finish.onmouseover = function(){if(hitwall === false){alert("You Win");}};
 }
 
 window.addEventListener("load",init);
