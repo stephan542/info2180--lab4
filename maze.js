1dc826a2150b39635f106bf41875fbabbe08375e
stephan542 <stefanwytter@gmail.com>
2017-10-11 03:06:01 -0500

    Excerise 1 Single boundary red

--- maze.js
+++ maze.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+function init(){
+	"use strict";
+	var x = document.getElementById("boundary1");
+	x.onmouseover = function(){
+		x.setAttribute("class","boundary youlose");
+	};
+	
+}
+
+window.addEventListener("load",init);
