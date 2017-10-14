e55fecaa1124e85f7cfb213d0cdcb5954ef86a59
stephan542 <stefanwytter@gmail.com>
2017-10-14 03:50:22 -0500

    Exercise 6 Disallowing cheating

--- maze.js
+++ maze.js
@@ -7,15 +7,9 @@ function checkbounds(bounds){
 		bound.onmouseover = function(){
 			bound.setAttribute("class","boundary youlose");
 			hitwall = true;
+			bounds[bounds.length-1].setAttribute("class","boundary example");
 		};
 	});
-	
-	bounds.forEach(function(bound){
-		bound.onmouseout = function(){
-			bound.setAttribute("class","boundary");
-		};
-	});
-	
 	
 }
 
@@ -23,11 +17,37 @@ function init(){
 	"use strict";
 	var boundary = document.querySelectorAll(".boundary");
 	checkbounds(boundary);
-	var start = document.getElementById("start");
-	start.onclick = function(){hitwall=false;};
-	var finish = document.getElementById("end");
-	finish.onmouseover = function(){if(hitwall === false){alert("You Win");}};
 	
+	var start = document.getElementById("start");
+	start.onclick = function(){
+		boundary.forEach(function(bound){
+				bound.setAttribute("class","boundary");
+				boundary[boundary.length-1].setAttribute("class","boundary example");
+		 });
+		hitwall = false;
+		document.getElementById("status").innerHTML= "Move throught the maze";
+	};
+	start.onmouseover = function(){
+		hitwall=false;
+	};
+	
+	var finish = document.getElementById("end");
+	finish.onmouseover = function(){
+		if(hitwall === false){
+			document.getElementById("status").innerHTML= "You Win";
+		}
+	};
+	finish.onmouseout = function(){document.getElementById("status").innerHTML= "Click S to restart";};
+	
+	var maze = document.getElementById("maze");
+	maze.onmouseleave = function(){
+		hitwall = true;
+		boundary.forEach(function(bound){
+				bound.setAttribute("class","boundary youlose");
+		 });
+		boundary[boundary.length-1].setAttribute("class","boundary example");
+		
+	};
 }
 
 window.addEventListener("load",init);
